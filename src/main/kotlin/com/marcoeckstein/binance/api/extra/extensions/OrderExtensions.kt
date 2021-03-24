a47563bd6e34cc0c05fc3cc6aa3343b137869cc6
Marco Eckstein <marco.eckstein@gmx.de>
2021-03-24 19:00:39 +0100

    fix: fix commit fc0195ee5328da6b9e12aeea007c9b12a6f30893

--- src/main/kotlin/com/marcoeckstein/binance/api/extra/extensions/OrderExtensions.kt
+++ src/main/kotlin/com/marcoeckstein/binance/api/extra/extensions/OrderExtensions.kt
@@ -2,18 +2,19 @@ package com.marcoeckstein.binance.api.extra.extensions
 
 import com.binance.api.client.domain.OrderSide
 import com.marcoeckstein.binance.api.client.prvt.account.Order
+import com.marcoeckstein.binance.api.client.prvt.account.OrderStatus
 import java.math.BigDecimal
 
 fun Order.lockedAmount(asset: String): BigDecimal =
     when (status) {
-        com.marcoeckstein.binance.api.client.prvt.account.OrderStatus.NEW ->
+        OrderStatus.NEW ->
             when {
                 side == OrderSide.BUY && quoteAsset == asset -> delegateMoney
                 side == OrderSide.SELL && baseAsset == asset -> origQty
                 else -> BigDecimal.ZERO
             }
-        com.marcoeckstein.binance.api.client.prvt.account.OrderStatus.PARTIAL_FILL, com.marcoeckstein.binance.api.client.prvt.account.OrderStatus.PENDING_CANCEL ->
+        OrderStatus.PARTIAL_FILL, OrderStatus.PENDING_CANCEL ->
             throw NotImplementedError()
-        com.marcoeckstein.binance.api.client.prvt.account.OrderStatus.FILLED, com.marcoeckstein.binance.api.client.prvt.account.OrderStatus.CANCELED, com.marcoeckstein.binance.api.client.prvt.account.OrderStatus.EXPIRED, com.marcoeckstein.binance.api.client.prvt.account.OrderStatus.REJECTED ->
+        OrderStatus.FILLED, OrderStatus.CANCELED, OrderStatus.EXPIRED, OrderStatus.REJECTED ->
             BigDecimal.ZERO
     }
--- src/main/kotlin/com/marcoeckstein/binance/api/extra/extensions/PaymentExtensions.kt
+++ src/main/kotlin/com/marcoeckstein/binance/api/extra/extensions/PaymentExtensions.kt
@@ -1,7 +1,8 @@
 package com.marcoeckstein.binance.api.extra.extensions
 
 import com.marcoeckstein.binance.api.client.prvt.account.Payment
+import com.marcoeckstein.binance.api.client.prvt.account.PaymentType
 import java.math.BigDecimal
 
 val Payment.cryptoCurrencyDelta: BigDecimal
-    get() = if (type == com.marcoeckstein.binance.api.client.prvt.account.PaymentType.BUY) obtainAmount else sourceAmount.negate()
+    get() = if (type == PaymentType.BUY) obtainAmount else sourceAmount.negate()
